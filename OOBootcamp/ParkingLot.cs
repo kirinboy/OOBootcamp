261a5dca166f86564e69adf9d0df338245090930
kirinboy <kirinboy@gmail.com>
2016-09-07 22:45:19 +0800

    ParkingLot: should fail to park car when a parking lot is full

--- OOBootcamp/ParkingLot.cs
+++ OOBootcamp/ParkingLot.cs
@@ -3,10 +3,21 @@
     public class ParkingLot
     {
         private Car car;
+        private int capacity;
+
+        public ParkingLot(int capacity)
+        {
+            this.capacity = capacity;
+        }
 
         public string Park(Car car)
         {
+            if (capacity == 0)
+            {
+                throw new ParkingLotFullException();
+            }
             this.car = car;
+            capacity--;
             return "1";
         }
 
--- OOBootcamp/ParkingLotFacts.cs
+++ OOBootcamp/ParkingLotFacts.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Xunit;
+﻿using Xunit;
 
 namespace OOBootcamp
 {
@@ -8,12 +7,28 @@ namespace OOBootcamp
         [Fact]
         public void should_be_able_to_pick_the_same_car_when_park_a_car_to_a_parking_lot()
         {
-            var parkingLot = new ParkingLot();
+            var parkingLot = new ParkingLot(1);
             var car = new Car();
 
             var ticket = parkingLot.Park(car);
 
             Assert.Same(car, parkingLot.Pick(ticket));
+        }
+
+        [Fact]
+        public void should_fail_to_park_car_when_a_parking_lot_is_full()
+        {
+            var parkingLot = new ParkingLot(1);
+            var car1 = new Car();
+            parkingLot.Park(car1);
+
+            var car2 = new Car();
+
+            Assert.Throws<ParkingLotFullException>(
+                () =>
+                {
+                    parkingLot.Park(car2);
+                });
         }
     }
 }
