6ce06001ffd96a50ab3ddae869dfb3a9fb024376
kirinboy <kirinboy@gmail.com>
2016-09-13 10:28:01 +0800

    ParkingBoy - Should be able to pick the same car from the parking lot when parking boy parked a car

---
+++ OOBootcamp/ParkingBoy.cs
@@ -0,0 +1,17 @@
+﻿namespace OOBootcamp
+{
+    public class ParkingBoy
+    {
+        private readonly ParkingLot parkingLot;
+
+        public ParkingBoy(ParkingLot parkingLot)
+        {
+            this.parkingLot = parkingLot;
+        }
+
+        public int Park(Car car)
+        {
+            return parkingLot.Park(car);
+        }
+    }
+}
---
+++ OOBootcamp/ParkingBoyFacts.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace OOBootcamp
+{
+    public class ParkingBoyFacts
+    {
+        [Fact]
+        public void should_be_able_to_pick_the_same_car_from_the_parking_lot_when_parking_boy_park_the_car()
+        {
+            var parkingLot = new ParkingLot(1);
+            var parkingBoy = new ParkingBoy(parkingLot);
+            var car = new Car();
+
+            var ticket = parkingBoy.Park(car);
+
+            Assert.Same(car, parkingLot.Pick(ticket));
+        }
+    }
+}
